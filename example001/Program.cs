3d9d01d3b9f7c155d7dce167329123c06002fcf9
Olga Rudson <o.rudson@mail.ru>
2022-10-26 22:15:27 +0300

    Число А в степени В

--- example001/Program.cs
+++ example001/Program.cs
@@ -4,6 +4,7 @@
 
 Console.WriteLine("Введите число А:");
 int numberA = int.Parse(Console.ReadLine()??"");
+
 Console.WriteLine("Введите число B:");
 int numberB = int.Parse(Console.ReadLine()??"");
 int result = numberA;
